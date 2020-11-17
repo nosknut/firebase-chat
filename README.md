4058ab10ac0f2ac7fc16bcd40c707367a1158c87
nosknut <nosknut@gmail.com>
2020-11-17 10:50:52 +0100

    Adds project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chat App using Firebase and React
+
+## Links
+* [Tutorial](https://www.youtube.com/watch?v=zQyrwxMPm88&t=472s&ab_channel=Fireship)
+* [Devops](https://www.youtube.com/watch?v=eB0nUzAI7M8&t=418s&ab_channel=Fireship)
---
+++ src/App.js
@@ -0,0 +1,114 @@
+import React, { useRef, useState } from 'react';
+import firebase from 'firebase';
+import "./App.css";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC7CSN9sTBzeqrdERl1msYKlTO_SP6QFVA",
+  authDomain: "nosknut-firebase-chat.firebaseapp.com",
+  databaseURL: "https://nosknut-firebase-chat.firebaseio.com",
+  projectId: "nosknut-firebase-chat",
+  storageBucket: "nosknut-firebase-chat.appspot.com",
+  messagingSenderId: "431091618442",
+  appId: "1:431091618442:web:5c31d5c7580185c6055373"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+function ChatMessage({ message }) {
+  const { text, uid, photoURL } = message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img alt="" src={photoURL} />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export function ChatRoom() {
+  const dummy = useRef();
+
+  const messageRef = firestore.collection('messages');
+  const query = messageRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState("");
+
+  async function sendMessage(e) {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messageRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+    setFormValue("");
+    dummy.current.scrollIntoView({ behaviour: 'smooth' });
+  }
+
+  return (
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={e => setFormValue(e.target.value)} placeholder="Say something nice" />
+
+        <button type="submit" disabled={!formValue}>✈️</button>
+      </form>
+    </>
+  )
+}
+
+function SignIn() {
+
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
+    </>
+  );
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
+    </div>
+  );
+}
+
+export default App;
